dea1565c4e457478bbd31926cc4212ac82580d24
the-Fish2 <nishkamk2@gmail.com>
2024-06-29 13:11:52 -0700

    so5 changes

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -100,14 +100,31 @@ theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
     rcases h2 with h2 | h2
     · rw [abs_of_nonneg h]
       exact h2
-    · rw []
+    · rw [] sorry
       exact h2
   constructor
 
 
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+    rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+    constructor
+    · intro h'
+      constructor
+      linarith
+      exact h'
+    . intro h'
+      rcases h' with ⟨h1, h2⟩
+      exact h2
+  . rw [abs_of_neg h]
+    constructor
+    · intro h'
+      constructor
+      · linarith
+      . linarith
+    . intro h'
+      linarith
 
 end MyAbs
 
@@ -128,25 +145,59 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, rfl | rfl⟩ <;> linarith [sq_nonneg x, sq_nonneg y]
+
+/- don't really get the solution here? had to go to solutions page for this -/
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    linarith
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    simp [eq_neg_iff_add_eq_zero.mpr, h1]
+  . left
+    simp [eq_of_sub_eq_zero, h1]
+  /- used basic template from solution for this as well: ie presence of h' and h'' -/
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    simp
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    simp [eq_neg_iff_add_eq_zero.mpr, h1]
+  . left
+    simp [eq_of_sub_eq_zero, h1]
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  . left
+    exact eq_of_sub_eq_zero h1
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  . left
+    exact eq_of_sub_eq_zero h1
 
-end
 
 example (P : Prop) : ¬¬P → P := by
   intro h
